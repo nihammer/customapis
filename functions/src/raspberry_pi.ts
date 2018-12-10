2568c31f94c8292fdf9c6812eee70861e79827d1
DINHNHAMNGUYEN <dinhnhamnguyen@gmail.com>
2018-12-11 08:48:13 +0900

    Add get ipaddress API

--- functions/src/raspberry_pi.ts
+++ functions/src/raspberry_pi.ts
@@ -1,9 +1,34 @@
 import * as functions from 'firebase-functions';
+import { firestore } from "firebase-admin";
 
 export class RaspberryPi {
+    private readonly fs: firestore.Firestore;
+
+    constructor() {
+        this.fs = firestore();
+    }
+
     registerIp = functions.https.onRequest((request, response) => {
         console.log("ip=  " + JSON.stringify(request.ip));
-        console.log("ips=  " + JSON.stringify(request.ips));
-        return response.send('OK');
+        return this.fs.collection('raspberry').doc('ip').set({ ip_address: request.ip })
+            .then(() => {
+                console.log("Document successfully written!");
+                return response.send('OK');
+            })
+            .catch((err) => {
+                console.error("Error: ", err);
+                return response.send('Failed!');
+            });
+    });
+
+    getIp = functions.https.onRequest((request, response) => {
+        return this.fs.collection('raspberry').doc('ip').get()
+            .then((snap) => {
+                return response.send(snap.data().ip_address);
+            })
+            .catch((err) => {
+                console.error("Error: ", err);
+                return response.send('Failed!');
+            });
     });
 }
